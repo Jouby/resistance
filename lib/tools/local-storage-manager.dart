f5e4ded2fe100b0d94e2e11948179ead4dc9a12f
aujou <aurelien.joubert@dnd.fr>
2020-03-30 14:21:18 +0200

    remove duplicate content

--- lib/tools/local-storage-manager.dart
+++ lib/tools/local-storage-manager.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class LocalStorageManager extends StatelessWidget {
-  //static List<List<dynamic>> data;
-
   static Future<dynamic> getLocaleStorageData(String key,
       [dynamic defaultValue]) async {
     final prefs = await SharedPreferences.getInstance();
@@ -32,23 +30,5 @@ abstract class LocalStorageManager extends StatelessWidget {
         prefs.setString(key, value.toString());
         break;
     }
-  }
-
-  static Route createRoute(Widget newPage) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => newPage,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.0, 5.0);
-        var end = Offset.zero;
-        var curve = Curves.ease;
-
-        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
   }
 }
